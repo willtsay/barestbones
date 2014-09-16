6cabea3d04e8795581fd19f98847af892164567e
William Tsay <willtsay@gmail.com>
2014-09-15 18:08:00 -0700

    added a room tracker

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 var io = require('socket.io')(process.env.PORT || 8080)
 
+var room = 0
+
 io.sockets.on('connection', function(socket){
+  socket.join(joinRoom(room))
   socket.on('echo', function (data){
     socket.emit('echo', data)
+  })
+  socket.on('get:room', function(){
+    var rooms = socket.rooms
+    socket.emit('get:room', rooms)
   })
   socket.on('change:color', function(data){
     io.sockets.emit('change:color', data)
   })
 })
+
+function joinRoom(room){
+  // generate a room
+  var roomToUse = room
+  room += 1
+  return roomToUse.toString()
+}
