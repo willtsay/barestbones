d45b97c5e0b4c8e036e212c2589044efe1559dcc
William Tsay <willtsay@gmail.com>
2014-09-19 11:03:34 -0700

    commented out some unused functions atm

--- app.js
+++ app.js
@@ -3,33 +3,76 @@ var io = require('socket.io')(process.env.PORT || 8080)
 var room = 0
 
 io.sockets.on('connection', function(socket){
-  socket.join(joinRoom(room))
-  socket.on('echo', function (data){
-    socket.emit('echo', data)
+  // leaves auto generated room that a socket automatically connects to
+  socket.leave(socket.rooms)
+  socket.on('join:room', function(room){
+    socket.join(room)
   })
   socket.on('get:room', function(){
     var rooms = socket.rooms
     socket.emit('get:room', rooms)
   })
-  socket.on('change:color', function(data){
-    io.sockets.emit('change:color', data)
+  socket.on("send:message:room",function(data){
+    io.sockets.in(data.room).emit('send:message:room', data.message)
   })
+
 })
 
-function joinRoom(room){
-  // generate a room
-  var roomToUse = room
-  room += 1
-  return roomToUse.toString()
-}
 
-roomHandler = {
-  room: 0,
-  joinRoom: function(){
-    roomToJoin = roomHandler.room
-    roomHandler.room+=1
-    return roomToJoin
-  }
-}
+
+
+
+
+// function findClientsSocketByRoomId(roomId) {
+// var res = []
+// , room = io.sockets.adapter.rooms[roomId];
+// if (room) {
+//     for (var id in room) {
+//     res.push(io.sockets.adapter.nsp.connected[id]);
+//     }
+// }
+// return res;
+// }
+
+// function Room(number){
+//   this.name = "r"+number
+//   this.roomNumber = number
+//   this.inRoom = 0
+//   this.add = function(){
+//     if (this.inRoom <2){
+//       this.inRoom++     
+//     }
+//   }
+//   this.leave = function(){
+//     this.inRoom--
+//   }
+// }
+
+// rH = {
+//   rooms: [],
+//   num: 0,
+//   getLowestUnoccupiedRoom: function(){
+//     rooms = Object.keys(io.sockets.adapter.rooms)
+//   },
+//   clientsInRoom: function(room){
+//     io.sockets.clients(room)
+//   },
+//   joinRoom: function(){
+//     var newRoom = new Room(rH.num)
+//     rH.rooms.push(newRoom)
+//     newRoom.add()
+//     rH.num+=1
+//     return newRoom.name
+//   },
+//   leaveRoom: function(){
+//     return 
+//   }
+// }
 
 // worried about room numbers getting huge?
+
+// assuming the server is always running -> joinRoomshould get all rooms w/ clients in it, and start from tehre.
+
+// objective: give lowest possible room number available. 
+
+// i can check if a key is in io.sockets.
