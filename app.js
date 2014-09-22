aac252d9c9fe2d4643c24a445d733b5122173d1f
William Tsay <willtsay@gmail.com>
2014-09-21 21:10:29 -0700

    implemented mutli board and timer

--- app.js
+++ app.js
@@ -6,100 +6,112 @@ io.sockets.on('connection', function(socket){
   // leaves auto generated room that a socket automatically connects to
   socket.leave(socket.rooms)
   socket.on('join:room', function(room){
-    socket.join(room)
+    if (rH.joinRoom(room, socket)){
+      rH.syncGame(room, socket)      
+    }
   })
-  socket.on('host:room', function(room){
-    rH.hostRoom(room)
+  socket.on('host:room', function(data){
+    rH.hostRoom(data, socket)
+    rH.initializeGame(data)
   })
   socket.on('get:room', function(){
     var rooms = socket.rooms
     socket.emit('get:room', rooms)
   })
   socket.on("send:message:room",function(data){
-    io.sockets.in(data.room).emit('send:message:room', data.message)
+    io.sockets.in(data.room).emit('send:message:room', data.message)  
   })
   socket.on('disconnect', function(){
-    room = socket.rooms[0]
-    rH.leaveRoom(room)
+    room = socket.room
+    rH.leaveRoom(room, socket)
   })
-
+  socket.on('send:update:room', function(data){
+    var room = socket.rooms[0]
+    io.sockets.in(room).emit('update:room', data)
+  })
+  socket.on('attempt:set', function(){
+    var data = {}
+    data.currentPlayer = socket.playerId
+    socket.emit('attempt:set', data)
+    io.sockets.in(room).emit('lock:players')
+  })
+  socket.on('update:timer', function(data){
+    var room = socket.rooms[0]
+    io.sockets.in(room).emit('update:timer', data)
+  })
+  socket.on('subUpdate:timer', function(timer){
+    var room = socket.rooms[0]
+    io.sockets.in(room).emit('subUpdate:timer', timer)
+  })
+  socket.on('reset:timers', function(data){
+    var room = socket.rooms[0]
+    io.sockets.in(room).emit('reset:timers', data)
+  })
+  socket.on('update:points', function(data){
+    var room = socket.rooms[0]
+    io.sockets.in(room).emit('update:points', data)
+  })
 })
 
 
 rH = {
   rooms: {},
-  joinRoom: function(room){
+  joinRoom: function(room, socket){
     var roomNames = Object.keys(rH.rooms)
-    if ($.inArray(room, roomNames) == 0){
-      return socket.emit('join:room:error')
+    if (!inArray(room, roomNames)){
+      socket.emit('join:room:error', room)
+      return false
+    } else if (rH.rooms[room].count >= 3){ 
+      socket.emit('join:room:error', room)
+      return false
     } else {
-      rH.rooms[room]+=1
+      var cRoom = rH.rooms[room]
+      cRoom.count++
+      cRoom.players[cRoom.count].active = true
+      socket.room = room
+      socket.playerId = cRoom.count
       socket.join(room)
+      io.sockets.in(room).emit('room:joined', cRoom)
+      return true
     }
   },
-  hostRoom: function(room){
-    // if the room is in the array of keys, stop
+  hostRoom: function(data, socket){
     var roomNames = Object.keys(rH.rooms)
-    if ($.inArray(room, roomNames) != 0 ){
-      return socket.emit('host:room:error')
+    if (inArray(data.room, roomNames)){
+      return socket.emit('host:room:error', data.room)
     } else {
-      rH.rooms[room] = 1
-      socket.join(room)
+      rH.rooms[data.room] = data.game
+      rH.rooms[data.room].count = 0
+      rH.rooms[data.room].players[0].active = true
+      socket.playerId = 0
+      socket.room = data.room
+      socket.join(data.room)
+      socket.emit('room:hosted', rH.rooms[data.room])
     }
-    checkRoomExistence(room)
   },
-  leaveRoom: function(room){
-    if ($.inArray(room, roomNames) != 0){
-      rH.rooms[room]--
-      if (rH.rooms[room] == 0){
+  leaveRoom: function(room, socket){
+    var roomNames =Object.keys(rH.rooms)
+    if (inArray(room, roomNames)){
+      rH.rooms[room].count--
+      if (rH.rooms[room].count < 0){
         delete rH.rooms[room]
       }
     }
     socket.leave(socket.rooms)
+  },
+  initializeGame: function(data){
+    rH.rooms[data.room] = data.game
+  },
+  syncGame: function(room, socket){
+    socket.emit('sync:game', rH.rooms[room])
   }
 }
 
-// host room vs join room.
-
-// function Room(number){
-//   this.name = "r"+number
-//   this.roomNumber = number
-//   this.inRoom = 0
-//   this.add = function(){
-//     if (this.inRoom <2){
-//       this.inRoom++     
-//     }
-//   }
-//   this.leave = function(){
-//     this.inRoom--
-//   }
-// }
-
-// rH = {
-//   rooms: [],
-//   num: 0,
-//   getLowestUnoccupiedRoom: function(){
-//     rooms = Object.keys(io.sockets.adapter.rooms)
-//   },
-//   clientsInRoom: function(room){
-//     io.sockets.clients(room)
-//   },
-//   joinRoom: function(){
-//     var newRoom = new Room(rH.num)
-//     rH.rooms.push(newRoom)
-//     newRoom.add()
-//     rH.num+=1
-//     return newRoom.name
-//   },
-//   leaveRoom: function(){
-//     return 
-//   }
-// }
-
-// worried about room numbers getting huge?
-
-// assuming the server is always running -> joinRoomshould get all rooms w/ clients in it, and start from tehre.
-
-// objective: give lowest possible room number available. 
-
-// i can check if a key is in io.sockets.
+function inArray(element, array){
+  for (i=0; i<array.length; i++){
+    if (array[i] == element){
+      return true
+    }
+  }
+  return false
+}
